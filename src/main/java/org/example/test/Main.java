a4f8ec263e328b60dbc69d516ad14900048668aa
Macbook <macook@Macbooks-MBP.Home>
2024-10-25 19:39:03 +0100

    Sou gostoso

--- src/main/java/org/example/test/Main.java
+++ src/main/java/org/example/test/Main.java
@@ -2,7 +2,8 @@ package org.example.test;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        System.out.println("Hello World");
+        System.out.println("O Rodrigo e gostoso");
     }
     // coisas tal coisas
 }
